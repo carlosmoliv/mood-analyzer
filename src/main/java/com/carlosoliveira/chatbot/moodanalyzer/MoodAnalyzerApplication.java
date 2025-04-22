3e3e49c170eb5975656da5734a4ff0e57511cf5b
Carlos Oliveira <carlosmoliveira93@gmail.com>
2025-04-21 23:11:10 -0300

    chore: start project

---
+++ src/main/java/com/carlosoliveira/chatbot/moodanalyzer/MoodAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.carlosoliveira.chatbot.moodanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoodAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carlosoliveira/chatbot/moodanalyzer/MoodAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosoliveira.chatbot.moodanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
